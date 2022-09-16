b9ab9846cf5dc757f81f203f8db493f1c396b088
Max Kuznetsov <mx.kuznetsov.dev@gmail.com>
2022-09-16 15:55:41 +0300

    upgrade swift to 5.7, AlertBuilder

---
+++ Sources/Common/Extensions/View+AlertStatus.swift
@@ -0,0 +1,27 @@
+import Foundation
+import SwiftUI
+
+public extension View {
+    func alert(status: Binding<AlertBuilder.AlertStatus>) -> some View {
+        let binding: Binding<AlertBuilder.AlertStatus?> = Binding(
+            get: { status.wrappedValue.status == .dismissed ? nil : status.wrappedValue },
+            set: { newValue in
+                if let newValue = newValue {
+                    status.wrappedValue = newValue
+                } else {
+                    status.wrappedValue = .dismissed
+                }
+            }
+        )
+
+        return self.alert(item: binding) { alertStatus in
+            switch alertStatus.status {
+            case .presented(let alertStyle):
+                return AlertBuilder.buildAlert(for: alertStyle)
+
+            default:
+                return Alert(title: Text(""))
+            }
+        }
+    }
+}
