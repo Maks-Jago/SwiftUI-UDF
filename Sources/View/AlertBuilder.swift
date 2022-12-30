adea907983b244fcbb7026be2b0939acfde54dd4
Max Kuznetsov <mx.kuznetsov.dev@gmail.com>
2022-12-30 16:53:39 +0200

    change title/text from String to closure

--- Sources/View/AlertBuilder.swift
+++ Sources/View/AlertBuilder.swift
@@ -19,7 +19,7 @@ public enum AlertBuilder {
             }
         }
 
-        public var id: Int
+        public var id: UUID
         public var status: Status
 
         public enum Status: Equatable {
@@ -65,7 +65,7 @@ public enum AlertBuilder {
         }
 
         public init() {
-            id = UUID().hashValue
+            id = UUID()
             status = .dismissed
         }
 
@@ -88,51 +88,79 @@ public enum AlertBuilder {
             lhs.id == rhs.id
         }
 
-        public var id: Int
+        public var id: UUID
         var type: AlertType
 
         enum AlertType {
-            case validationError(text: String)
-            case success(text: String)
-            case failure(text: String)
-            case message(text: String)
-            case messageTitle(title: String, message: String)
-            case custom(title: String, text: String, primaryButton: Alert.Button, secondaryButton: Alert.Button)
-            case customDismiss(title: String, text: String, dismissButton: Alert.Button)
+            case validationError(text: () -> String)
+            case success(text: () -> String)
+            case failure(text: () -> String)
+            case message(text: () -> String)
+            case messageTitle(title: () -> String, message: () -> String)
+            case custom(title: () -> String, text: () -> String, primaryButton: Alert.Button, secondaryButton: Alert.Button)
+            case customDismiss(title: () -> String, text: () -> String, dismissButton: Alert.Button)
         }
 
-        public init(validationError text: String) {
-            id = (text + UUID().uuidString).hashValue
+        public init(validationError text: @autoclosure @escaping () -> String) {
+            self.init(validationError: text)
+        }
+
+        public init(validationError text: @escaping () -> String) {
+            id = UUID()
             type = .validationError(text: text)
         }
 
-        public init(failure text: String) {
-            id = (text + UUID().uuidString).hashValue
+        public init(failure text: @autoclosure @escaping () -> String) {
+            self.init(failure: text)
+        }
+
+        public init(failure text: @escaping () -> String) {
+            id = UUID()
             type = .failure(text: text)
         }
 
-        public init(success text: String) {
-            id = (text + UUID().uuidString).hashValue
+        public init(success text: @autoclosure @escaping () -> String) {
+            self.init(success: text)
+        }
+
+        public init(success text: @escaping () -> String) {
+            id = UUID()
             type = .success(text: text)
         }
 
-        public init(message text: String) {
-            id = (text + UUID().uuidString).hashValue
+        public init(message text: @autoclosure @escaping () -> String) {
+            self.init(message: text)
+        }
+
+        public init(message text: @escaping () -> String) {
+            id = UUID()
             type = .message(text: text)
         }
 
-        public init(title: String, message: String) {
-            id = (title + message + UUID().uuidString).hashValue
+        public init(title: @autoclosure @escaping () -> String, message: @autoclosure @escaping () -> String) {
+            self.init(title: title, message: message)
+        }
+
+        public init(title: @escaping () -> String, message: @escaping () -> String) {
+            id = UUID()
             type = .messageTitle(title: title, message: message)
         }
 
-        public init(title: String, text: String, primaryButton: Alert.Button, secondaryButton: Alert.Button) {
-            id = (title + text + UUID().uuidString).hashValue
+        public init(title: @autoclosure @escaping () -> String, text: @autoclosure @escaping () -> String, primaryButton: Alert.Button, secondaryButton: Alert.Button) {
+            self.init(title: title, text: text, primaryButton: primaryButton, secondaryButton: secondaryButton)
+        }
+
+        public init(title: @escaping () -> String, text: @escaping () -> String, primaryButton: Alert.Button, secondaryButton: Alert.Button) {
+            id = UUID()
             type = .custom(title: title, text: text, primaryButton: primaryButton, secondaryButton: secondaryButton)
         }
 
-        public init(title: String, text: String, dismissButton: Alert.Button) {
-            id = (title + text + UUID().uuidString).hashValue
+        public init(title: @autoclosure @escaping () -> String, text: @autoclosure @escaping () -> String, dismissButton: Alert.Button) {
+            self.init(title: title, text: text, dismissButton: dismissButton)
+        }
+
+        public init(title: @escaping () -> String, text: @escaping () -> String, dismissButton: Alert.Button) {
+            id = UUID()
             type = .customDismiss(title: title, text: text, dismissButton: dismissButton)
         }
     }
@@ -140,25 +168,25 @@ public enum AlertBuilder {
     public static func buildAlert(for style: AlertStyle) -> Alert {
         switch style.type {
         case .validationError(let text):
-            return alert(title: NSLocalizedString("Incorrect input", comment: "Validation error"), text: text)
+            return alert(title: NSLocalizedString("Incorrect input", comment: "Validation error"), text: text())
 
         case .success(let text):
-            return alert(title: NSLocalizedString("Success", comment: "Success"), text: text)
+            return alert(title: NSLocalizedString("Success", comment: "Success"), text: text())
 
         case .failure(let text):
-            return alert(title: NSLocalizedString("Error", comment: "Error"), text: text)
+            return alert(title: NSLocalizedString("Error", comment: "Error"), text: text())
 
         case .message(let text):
-            return alert(title: "", text: text)
+            return alert(title: "", text: text())
 
         case .messageTitle(let title, let text):
-            return alert(title: title, text: text)
+            return alert(title: title(), text: text())
 
         case let .customDismiss(title, text, dismissButton):
-            return alert(title: title, text: text, dismissButton: dismissButton)
+            return alert(title: title(), text: text(), dismissButton: dismissButton)
 
         case let .custom(title, text, primaryButton, secondaryButton):
-            return alert(title: title, text: text, primaryButton: primaryButton, secondaryButton: secondaryButton)
+            return alert(title: title(), text: text(), primaryButton: primaryButton, secondaryButton: secondaryButton)
         }
     }
 
