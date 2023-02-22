0a41ca840fa6ef4e5bfc987ba18c8fc9f76de93b
Max Kuznetsov <mx.kuznetsov.dev@gmail.com>
2023-02-22 20:19:15 +0200

    add LogOptions to Store, add mapAction to Middlewares

--- Sources/Store/Loggers/EnvironmentStore+ConsoleLogger.swift
+++ Sources/Store/Loggers/EnvironmentStore+ConsoleLogger.swift
@@ -1,11 +1,11 @@
 import Foundation
 
 public extension EnvironmentStore {
-    convenience init(initial state: State, accessToken: String) throws {
-        try self.init(initial: state, accessToken: accessToken, logger: ConsoleDebugLogger(options: .all))
+    convenience init(initial state: State, accessToken: String, logOptions: LogDistributor.Options) throws {
+        try self.init(initial: state, accessToken: accessToken, logger: ConsoleDebugLogger(options: .all), logOptions: logOptions)
     }
 
-    convenience init(initial state: State, debugLoggerOptions options: ConsoleDebugLogger.Options, accessToken: String) throws {
-        try self.init(initial: state, accessToken: accessToken, logger: ConsoleDebugLogger(options: options))
+    convenience init(initial state: State, consoleLoggerOptions: ConsoleDebugLogger.Options, accessToken: String, logOptions: LogDistributor.Options) throws {
+        try self.init(initial: state, accessToken: accessToken, logger: ConsoleDebugLogger(options: consoleLoggerOptions), logOptions: logOptions)
     }
 }
