d70614f1a7c0a600433e5961b9652ffa0f7dfdb7
Lena Soroka <4ta.lenasoroka.1989@gmail.com>
2023-09-06 16:40:51 +0300

    added extension for view to update navigation

---
+++ Sources/Common/Extensions/View+NavigationDestination.swift
@@ -0,0 +1,37 @@
+//
+//  View+NavigationDestination.swift
+//  
+//
+//  Created by Lena Soroka on 06.09.2023.
+//
+
+import SwiftUI
+
+public extension View {
+    @available(iOS 16.0, *)
+    func navigationDestination<R: Routing>(router: Router<R>, selectedRoute: Binding<R.Route?>) -> some View {
+        self.navigationDestination(isPresented: selectedRoute.isPresented()) {
+            if let route = selectedRoute.wrappedValue {
+                router.view(for: route)
+            }
+        }
+    }
+}
+
+fileprivate extension Binding {
+    func isPresented<T>() -> Binding<Bool> where Value == Optional<T> {
+        Binding<Bool>(
+            get: {
+                switch self.wrappedValue {
+                case .some: return true
+                case .none: return false
+                }
+            },
+            set: {
+                if !$0 {
+                    self.wrappedValue = nil
+                }
+            }
+        )
+    }
+}
