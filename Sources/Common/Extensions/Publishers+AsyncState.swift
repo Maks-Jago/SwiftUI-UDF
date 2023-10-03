63aa3bec22b45e5cae666c137629b350464a6056
Max Kuznetsov <mx.kuznetsov.dev@gmail.com>
2023-10-03 17:56:38 +0300

    fix IsolatedState error on Xcode 15

--- Sources/Common/Extensions/Publishers+AsyncState.swift
+++ Sources/Common/Extensions/Publishers+AsyncState.swift
@@ -2,7 +2,7 @@ import Foundation
 import Combine
 
 public extension Publishers {
-    static func IsolatedState<State: AppReducer>(from store: some Store<State>) -> AnyPublisher<State, Never> {
+    static func IsolatedState<State: AppReducer>(from store: any Store<State>) -> AnyPublisher<State, Never> {
         Future { promise in
             Task {
                 let immutableState = await store.state
