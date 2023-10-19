48d9dc642ddd6c0dda4b85e53dd49934001d8991
Max Kuznetsov <mx.kuznetsov.dev@gmail.com>
2023-10-19 23:54:59 +0300

    changes

--- Sources/Middleware/EnvironmentMiddleware.swift
+++ Sources/Middleware/EnvironmentMiddleware.swift
@@ -2,8 +2,9 @@
 import UDFCore
 import Foundation
 
-public protocol EnvironmentMiddleware<State>: Middleware {
+public protocol EnvironmentMiddleware {
     associatedtype Environment
+    associatedtype State: AppReducer
 
     var environment: Environment! { get set }
 
@@ -19,33 +20,3 @@ public extension EnvironmentMiddleware where Environment == Void {
     static func buildLiveEnvironment(for store: some Store<State>) -> Environment { () }
     static func buildTestEnvironment(for store: some Store<State>) -> Environment { () }
 }
-
-public extension EnvironmentMiddleware {
-    init(store: some Store<State>) {
-        if ProcessInfo.processInfo.xcTest {
-            self.init(store: store, environment: Self.buildTestEnvironment(for: store))
-        } else {
-            self.init(store: store, environment: Self.buildLiveEnvironment(for: store))
-        }
-    }
-
-    init(store: some Store<State>, queue: DispatchQueue) {
-        if ProcessInfo.processInfo.xcTest {
-            self.init(store: store, environment: Self.buildTestEnvironment(for: store), queue: queue)
-        } else {
-            self.init(store: store, environment: Self.buildLiveEnvironment(for: store), queue: queue)
-        }
-    }
-
-    init(store: some Store<State>, environment: Environment) {
-        let queueLabel = String(describing: Self.self)
-        self.init(store: store, environment: environment, queue: DispatchQueue(label: queueLabel))
-    }
-
-    init(store: some Store<State>, environment: Environment, queue: DispatchQueue) {
-        self.init(store: store, queue: queue)
-        self.environment = environment
-    }
-}
-
-
