867da9dafa356db0e4fde50d1a539a3bb3748ba4
Max Kuznetsov <mx.kuznetsov.dev@gmail.com>
2023-10-19 23:07:46 +0300

    EnvironmentMiddleware default inits

--- Sources/Middleware/EnvironmentMiddleware.swift
+++ Sources/Middleware/EnvironmentMiddleware.swift
@@ -20,3 +20,33 @@ public extension EnvironmentMiddleware where Environment == Void {
     static func buildLiveEnvironment(for store: some Store<State>) -> Environment { () }
     static func buildTestEnvironment(for store: some Store<State>) -> Environment { () }
 }
+
+public extension EnvironmentMiddleware {
+    init(store: some Store<State>) {
+        if ProcessInfo.processInfo.xcTest {
+            self.init(store: store, environment: Self.buildTestEnvironment(for: store))
+        } else {
+            self.init(store: store, environment: Self.buildLiveEnvironment(for: store))
+        }
+    }
+
+    init(store: some Store<State>, queue: DispatchQueue) {
+        if ProcessInfo.processInfo.xcTest {
+            self.init(store: store, environment: Self.buildTestEnvironment(for: store), queue: queue)
+        } else {
+            self.init(store: store, environment: Self.buildLiveEnvironment(for: store), queue: queue)
+        }
+    }
+
+    init(store: some Store<State>, environment: Environment) {
+        let queueLabel = String(describing: Self.self)
+        self.init(store: store, environment: environment, queue: DispatchQueue(label: queueLabel))
+    }
+
+    init(store: some Store<State>, environment: Environment, queue: DispatchQueue) {
+        self.init(store: store, queue: queue)
+        self.environment = environment
+    }
+}
+
+
--- Sources/Middleware/Middleware.swift
+++ Sources/Middleware/Middleware.swift
@@ -9,30 +9,32 @@ public extension Middleware {
     }
 }
 
-public extension Middleware where Self: EnvironmentMiddleware {
-    init(store: some Store<State>) {
-        if ProcessInfo.processInfo.xcTest {
-            self.init(store: store, environment: Self.buildTestEnvironment(for: store))
-        } else {
-            self.init(store: store, environment: Self.buildLiveEnvironment(for: store))
-        }
-    }
-
-    init(store: some Store<State>, queue: DispatchQueue) {
-        if ProcessInfo.processInfo.xcTest {
-            self.init(store: store, environment: Self.buildTestEnvironment(for: store), queue: queue)
-        } else {
-            self.init(store: store, environment: Self.buildLiveEnvironment(for: store), queue: queue)
-        }
-    }
-
-    init(store: some Store<State>, environment: Environment) {
-        let queueLabel = String(describing: Self.self)
-        self.init(store: store, environment: environment, queue: DispatchQueue(label: queueLabel))
-    }
-
-    init(store: some Store<State>, environment: Environment, queue: DispatchQueue) {
-        self.init(store: store, queue: queue)
-        self.environment = environment
-    }
-}
+//public extension Middleware where Self: EnvironmentMiddleware {
+//    init(store: some Store<State>) {
+//        if ProcessInfo.processInfo.xcTest {
+//            self.init(store: store, environment: Self.buildTestEnvironment(for: store))
+//        } else {
+//            self.init(store: store, environment: Self.buildLiveEnvironment(for: store))
+//        }
+//    }
+//
+//    init(store: some Store<State>, queue: DispatchQueue) {
+//        if ProcessInfo.processInfo.xcTest {
+//            self.init(store: store, environment: Self.buildTestEnvironment(for: store), queue: queue)
+//        } else {
+//            self.init(store: store, environment: Self.buildLiveEnvironment(for: store), queue: queue)
+//        }
+//    }
+//
+//    init(store: some Store<State>, environment: Environment) {
+//        let queueLabel = String(describing: Self.self)
+//        self.init(store: store, environment: environment, queue: DispatchQueue(label: queueLabel))
+//    }
+//
+//    init(store: some Store<State>, environment: Environment, queue: DispatchQueue) {
+//        self.init(store: store, queue: queue)
+//        self.environment = environment
+//    }
+//}
+//
+//
