11821a6755b9032913f79d0995e553410f35de0a
Max Kuznetsov <mx.kuznetsov.dev@gmail.com>
2023-11-19 20:48:09 +0200

    Swift 5.9

--- Sources/Common/Extensions/Publishers+AsyncState.swift
+++ Sources/Common/Extensions/Publishers+AsyncState.swift
@@ -3,10 +3,12 @@ import Combine
 
 public extension Publishers {
     static func IsolatedState<State: AppReducer>(from store: any Store<State>) -> AnyPublisher<State, Never> {
-        Future { promise in
-            Task.detached(priority: .high) {
-                let immutableState = await store.state
-                promise(.success(immutableState))
+        Deferred {
+            Future { promise in
+                Task.detached(priority: .high) {
+                    let immutableState = await store.state
+                    promise(.success(immutableState))
+                }
             }
         }
         .eraseToAnyPublisher()
