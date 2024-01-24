4f5e9b62bc456be1d9b025d91755d63c7c031bfa
Max Kuznetsov <mx.kuznetsov.dev@gmail.com>
2024-01-25 00:13:39 +0200

    update error code default value, add new action

--- Sources/Store/Actions/Actions.swift
+++ Sources/Store/Actions/Actions.swift
@@ -26,7 +26,7 @@ public extension Actions {
         public init() {}
     }
 
-    struct Error: Action {
+    struct Error: Action, LocalizedError {
         public static func == (lhs: Actions.Error, rhs: Actions.Error) -> Bool {
             lhs.error == rhs.error && lhs.id == rhs.id
         }
@@ -36,12 +36,18 @@ public extension Actions {
         public var code: Int
         public var meta: [String: Any]?
 
-        public init<Id: Hashable>(error: String? = nil, id: Id, code: Int = -1001, meta: [String: Any]? = nil) {
+        public init<Id: Hashable>(error: String? = nil, id: Id, code: Int? = nil, meta: [String: Any]? = nil) {
             self.error = error?.isEmpty == true ? nil : error
             self.id = AnyHashable(id)
             self.meta = meta
-            self.code = code
+            self.code = if let code {
+                code
+            } else {
+                error?.hashValue ?? -1001
+            }
         }
+
+        public var errorDescription: String? { error }
     }
 
     struct Message: Action {
@@ -81,6 +87,8 @@ public extension Actions {
             self.cancelation = AnyHashable(cancelation)
         }
     }
+
+    struct ApplicationDidReceiveMemoryWarning: Action {}
 }
 
 
