53c45d262310743d877d23e1d73dde59d69862e9
Max Kuznetsov <mx.kuznetsov.dev@gmail.com>
2024-02-03 19:18:40 +0200

    swift 5.9.2

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.8
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -16,7 +16,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-collections", from: "1.0.4")
+        .package(url: "https://github.com/apple/swift-collections", from: "1.0.6")
     ],
     targets: [
         .target(
