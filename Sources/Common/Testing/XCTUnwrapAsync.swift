40bdf25838c9e84097736e09cd0ffc58de4cc10c
Max Kuznetsov <mx.kuznetsov.dev@gmail.com>
2024-02-25 15:19:40 +0200

    add canImport to XCTUnwrapAsync

--- Sources/Common/Testing/XCTUnwrapAsync.swift
+++ Sources/Common/Testing/XCTUnwrapAsync.swift
@@ -5,9 +5,11 @@
 //  Created by Arthur Zavolovych on 13.02.2024.
 //
 
+#if canImport(XCTest)
 import XCTest
 
 public func XCTUnwrapAsync<T>(_ expression: @autoclosure () async throws -> T?, _ message: @autoclosure () -> String = "", file: StaticString = #filePath, line: UInt = #line) async throws -> T {
     let res = try await expression()
     return try XCTUnwrap(res, message(), file: file, line: line)
 }
+#endif
