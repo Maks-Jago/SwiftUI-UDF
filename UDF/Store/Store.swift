8e8a1834013e582f7df8bc9184b515a77b10995e
Max Kuznetsov <mx.kuznetsov.dev@gmail.com>
2024-05-25 19:31:55 +0300

    add Command with options (up to 5), add extra bind methods to store

--- UDF/Store/Store.swift
+++ UDF/Store/Store.swift
@@ -61,4 +61,76 @@ public extension Store {
             )
         }
     }
+
+    func bind<T1, T2>(
+        _ action: @escaping (T1, T2) -> some Action,
+        priority: ActionPriority = .default,
+        fileName: String = #file,
+        functionName: String = #function,
+        lineNumber: Int = #line
+    ) -> CommandWith2<T1, T2> {
+        return { v1, v2 in
+            self.dispatch(
+                action(v1, v2),
+                priority: priority,
+                fileName: fileName,
+                functionName: functionName,
+                lineNumber: lineNumber
+            )
+        }
+    }
+
+    func bind<T1, T2, T3>(
+        _ action: @escaping (T1, T2, T3) -> some Action,
+        priority: ActionPriority = .default,
+        fileName: String = #file,
+        functionName: String = #function,
+        lineNumber: Int = #line
+    ) -> CommandWith3<T1, T2, T3> {
+        return { v1, v2, v3 in
+            self.dispatch(
+                action(v1, v2, v3),
+                priority: priority,
+                fileName: fileName,
+                functionName: functionName,
+                lineNumber: lineNumber
+            )
+        }
+    }
+
+    func bind<T1, T2, T3, T4>(
+        _ action: @escaping (T1, T2, T3, T4) -> some Action,
+        priority: ActionPriority = .default,
+        fileName: String = #file,
+        functionName: String = #function,
+        lineNumber: Int = #line
+    ) -> CommandWith4<T1, T2, T3, T4> {
+        return { v1, v2, v3, v4 in
+            self.dispatch(
+                action(v1, v2, v3, v4),
+                priority: priority,
+                fileName: fileName,
+                functionName: functionName,
+                lineNumber: lineNumber
+            )
+        }
+    }
+
+    func bind<T1, T2, T3, T4, T5>(
+        _ action: @escaping (T1, T2, T3, T4, T5) -> some Action,
+        priority: ActionPriority = .default,
+        fileName: String = #file,
+        functionName: String = #function,
+        lineNumber: Int = #line
+    ) -> CommandWith5<T1, T2, T3, T4, T5> {
+        return { v1, v2, v3, v4, v5 in
+            self.dispatch(
+                action(v1, v2, v3, v4, v5),
+                priority: priority,
+                fileName: fileName,
+                functionName: functionName,
+                lineNumber: lineNumber
+            )
+        }
+    }
 }
