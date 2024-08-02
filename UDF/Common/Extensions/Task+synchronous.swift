22954b2d88821c35caf9e89916ff7642b9589100
Max Kuznetsov <mx.kuznetsov.dev@gmail.com>
2024-08-02 14:29:39 +0300

    Create Task+synchronous.swift

---
+++ UDF/Common/Extensions/Task+synchronous.swift
@@ -0,0 +1,18 @@
+
+import Foundation
+
+public extension Task where Failure == Error {
+    /// Performs an async task in a sync context.
+    ///
+    /// - Note: This function blocks the thread until the given operation is finished. The caller is responsible for managing multithreading.
+    static func synchronous(priority: TaskPriority? = nil, operation: @escaping @Sendable () async throws -> Success) {
+        let semaphore = DispatchSemaphore(value: 0)
+
+        Task(priority: priority) {
+            defer { semaphore.signal() }
+            return try await operation()
+        }
+
+        semaphore.wait()
+    }
+}
