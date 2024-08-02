5b5395101377c39443f889fc62d5eaf98dc38fa6
Max Kuznetsov <mx.kuznetsov.dev@gmail.com>
2024-08-02 17:13:07 +0300

    update Store subscribe methods

--- UDF/Store/EnvironmentStore.swift
+++ UDF/Store/EnvironmentStore.swift
@@ -123,92 +123,101 @@ extension EnvironmentStore {
 // MARK: - Subscribe Methods
 public extension EnvironmentStore {
 
-    func subscribe<M: Middleware<State>>(_ middlewareType: M.Type) async where M.State == State, M: EnvironmentMiddleware {
+    func subscribe<M: Middleware<State>>(_ middlewareType: M.Type) where M.State == State, M: EnvironmentMiddleware {
         if ProcessInfo.processInfo.xcTest {
-            await self.subscribe { store in
+            self.subscribe { store in
                 middlewareType.init(store: store, environment: M.buildTestEnvironment(for: store))
             }
         } else {
-            await self.subscribe { store in
+            self.subscribe { store in
                 middlewareType.init(store: store, environment: M.buildLiveEnvironment(for: store))
             }
         }
     }
 
-    func subscribe<M: Middleware<State>>(_ middlewareType: M.Type) async where M.State == State {
-        await self.subscribe { store in
+    func subscribe<M: Middleware<State>>(_ middlewareType: M.Type) where M.State == State {
+        self.subscribe { store in
             middlewareType.init(store: store)
         }
     }
 
-    func subscribe<M: Middleware<State>>(_ middlewareType: M.Type, environment: M.Environment) async where M.State == State, M: EnvironmentMiddleware {
-        await self.subscribe { store in
+    func subscribe<M: Middleware<State>>(_ middlewareType: M.Type, environment: M.Environment) where M.State == State, M: EnvironmentMiddleware {
+        self.subscribe { store in
             middlewareType.init(store: store, environment: environment)
         }
     }
 
-    func subscribe<M: Middleware<State>>(_ middlewareType: M.Type, on queue: DispatchQueue) async where M.State == State, M: EnvironmentMiddleware {
+    func subscribe<M: Middleware<State>>(_ middlewareType: M.Type, on queue: DispatchQueue) where M.State == State, M: EnvironmentMiddleware {
         if ProcessInfo.processInfo.xcTest {
-            await self.subscribe { store in
+            self.subscribe { store in
                 middlewareType.init(store: store, environment: M.buildTestEnvironment(for: store), queue: queue)
             }
         } else {
-            await self.subscribe { store in
+            self.subscribe { store in
                 middlewareType.init(store: store, environment: M.buildLiveEnvironment(for: store), queue: queue)
             }
         }
     }
 
-    func subscribe<M: Middleware<State>>(_ middlewareType: M.Type, on queue: DispatchQueue) async where M.State == State {
-        await self.subscribe { store in
+    func subscribe<M: Middleware<State>>(_ middlewareType: M.Type, on queue: DispatchQueue) where M.State == State {
+        self.subscribe { store in
             middlewareType.init(store: store, queue: queue)
         }
     }
 
-    func subscribe<M: Middleware<State>>(_ middlewareType: M.Type, environment: M.Environment, on queue: DispatchQueue) async where M.State == State, M: EnvironmentMiddleware {
-        await self.subscribe { store in
+    func subscribe<M: Middleware<State>>(_ middlewareType: M.Type, environment: M.Environment, on queue: DispatchQueue) where M.State == State, M: EnvironmentMiddleware {
+        self.subscribe { store in
             middlewareType.init(store: store, environment: environment, queue: queue)
         }
     }
 
     func subscribeAsync<M>(_ middlewareType: M.Type, onSubscribe: @escaping () -> Void = {}) where M : Middleware, M: EnvironmentMiddleware, State == M.State {
-        Task(priority: .userInitiated) {
-            await subscribe(middlewareType)
-            onSubscribe()
+        let environment = if ProcessInfo.processInfo.xcTest {
+            M.buildTestEnvironment(for: store)
+        } else {
+            M.buildLiveEnvironment(for: store)
         }
+
+        subscribeAsync(middlewareType, environment: environment, onSubscribe: onSubscribe)
     }
 
     func subscribeAsync<M>(_ middlewareType: M.Type, onSubscribe: @escaping () -> Void = {}) where M : Middleware, State == M.State {
         Task(priority: .userInitiated) {
-            await subscribe(middlewareType)
+            await self.store.subscribe(middlewareType.init(store: self.store))
             onSubscribe()
         }
     }
 
     func subscribeAsync<M>(_ middlewareType: M.Type, environment: M.Environment, onSubscribe: @escaping () -> Void = {}) where M: Middleware, State == M.State, M: EnvironmentMiddleware {
         Task(priority: .userInitiated) {
-            await subscribe(middlewareType, environment: environment)
+            await self.store.subscribe(middlewareType.init(store: self.store, environment: environment))
             onSubscribe()
         }
     }
 
     func subscribeAsync<M>(_ middlewareType: M.Type, on queue: DispatchQueue, onSubscribe: @escaping () -> Void = {}) where M: Middleware, State == M.State, M: EnvironmentMiddleware {
+        let environment = if ProcessInfo.processInfo.xcTest {
+            M.buildTestEnvironment(for: store)
+        } else {
+            M.buildLiveEnvironment(for: store)
+        }
+
         Task(priority: .userInitiated) {
-            await subscribe(middlewareType, on: queue)
+            await store.subscribe(middlewareType.init(store: self.store, environment: environment, queue: queue))
             onSubscribe()
         }
     }
 
     func subscribeAsync<M>(_ middlewareType: M.Type, on queue: DispatchQueue, onSubscribe: @escaping () -> Void = {}) where M: Middleware, State == M.State {
         Task(priority: .userInitiated) {
-            await subscribe(middlewareType, on: queue)
+            await self.store.subscribe(middlewareType.init(store: store, queue: queue))
             onSubscribe()
         }
     }
 
     func subscribeAsync<M>(_ middlewareType: M.Type, environment: M.Environment, on queue: DispatchQueue, onSubscribe: @escaping () -> Void = {}) where M: Middleware, State == M.State, M: EnvironmentMiddleware {
         Task(priority: .userInitiated) {
-            await subscribe(middlewareType, environment: environment, on: queue)
+            await self.store.subscribe(middlewareType.init(store: self.store, environment: environment, queue: queue))
             onSubscribe()
         }
     }
@@ -223,10 +232,16 @@ public extension EnvironmentStore {
         }
     }
 
-    func subscribe(@MiddlewareBuilder<State> build: @escaping (_ store: any Store<State>) -> [MiddlewareWrapper<State>]) async {
+    func subscribe(@MiddlewareBuilder<State> build: @escaping (_ store: any Store<State>) -> [MiddlewareWrapper<State>]) {
+        Task.synchronous(priority: .userInitiated) {
+            await self._subscribe(build: build)
+        }
+    }
+
+    private func _subscribe(@MiddlewareBuilder<State> build: @escaping (_ store: any Store<State>) -> [MiddlewareWrapper<State>]) async {
         await self.store.subscribe(
-            build(store).map { wrapper in
-                wrapper.instance ?? self.middleware(store: store, type: wrapper.type)
+            build(self.store).map { wrapper in
+                wrapper.instance ?? self.middleware(store: self.store, type: wrapper.type)
             }
         )
     }
