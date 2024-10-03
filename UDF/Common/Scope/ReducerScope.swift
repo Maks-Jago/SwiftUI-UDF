f9a39c942c2c5151b98ba7fcaa8f1e411fda5590
Oksana Fedorchuk <oksanafed.dev@gmail.com>
2024-10-03 15:50:59 +0300

    Scopes, common

--- UDF/Common/Scope/ReducerScope.swift
+++ UDF/Common/Scope/ReducerScope.swift
@@ -1,20 +1,36 @@
+//===--- ReducerScope.swift ---------------------------------------===//
 //
-//  ReducerScope.swift
-//  
+// This source file is part of the UDF open source project
 //
-//  Created by Max Kuznetsov on 07.11.2021.
+// Copyright (c) 2024 You Are Launched
+// Licensed under Apache License v2.0
 //
+// See https://opensource.org/licenses/Apache-2.0 for license information
+//
+//===----------------------------------------------------------------------===//
 
 import Foundation
 
+/// A class representing a scope that contains a `Reducible` reducer.
+/// This is used to encapsulate a reducer within a specific scope, providing
+/// methods for equality checks and management of the reducer instance.
 final class ReducerScope<R: Reducible>: EquatableScope {
-    static func == (lhs: ReducerScope<R>, rhs: ReducerScope<R>) -> Bool {
-        lhs.reducer == rhs.reducer
-    }
-
+    
+    /// The reducer instance that this scope manages.
     var reducer: R?
-
+    
+    /// Initializes a new `ReducerScope` with the provided reducer.
+    /// - Parameter reducer: An optional `Reducible` instance to be managed by this scope.
     init(reducer: R?) {
         self.reducer = reducer
+    }
+    
+    /// Checks for equality between two `ReducerScope` instances.
+    /// - Parameters:
+    ///   - lhs: The left-hand side `ReducerScope` to compare.
+    ///   - rhs: The right-hand side `ReducerScope` to compare.
+    /// - Returns: A Boolean value indicating whether the two scopes are equal.
+    static func == (lhs: ReducerScope<R>, rhs: ReducerScope<R>) -> Bool {
+        lhs.reducer == rhs.reducer
     }
 }
--- UDF/Common/SubscribersCoordinator.swift
+++ UDF/Common/SubscribersCoordinator.swift
@@ -1,21 +1,51 @@
+//===--- SubscribersCoordinator.swift -----------------------------===//
+//
+// This source file is part of the UDF open source project
+//
+// Copyright (c) 2024 You Are Launched
+// Licensed under Apache License v2.0
+//
+// See https://opensource.org/licenses/Apache License v2.0 for license information
+//
+//===----------------------------------------------------------------------===//
+
 import Foundation
 import SwiftUI
 
+/// A type alias representing a subscriber that listens to state changes in the `AppReducer`.
+/// - Parameters:
+///   - oldState: The previous state before the change.
+///   - newState: The new state after the change.
+///   - animation: An optional animation that can be applied to the state change.
 typealias StateSubscriber<State: AppReducer> = (_ oldState: State, _ newState: State, _ animation: Animation?) -> Void
 
+/// An actor responsible for coordinating subscribers that respond to state changes.
 actor SubscribersCoordinator<T> {
+    /// A dictionary of subscribers, stored by unique keys.
     private var subscribers: [String: T] = [:]
-
+    
+    /// Adds a subscriber to the coordinator.
+    ///
+    /// - Parameters:
+    ///   - subscriber: The subscriber to be added.
+    ///   - key: An optional key to identify the subscriber. If not provided, a new UUID string is used.
+    /// - Returns: The key associated with the added subscriber.
     @discardableResult
     func add(subscriber: T, for key: String = UUID().uuidString) -> String {
         subscribers[key] = subscriber
         return key
     }
-
+    
+    /// Removes a subscriber for the given key.
+    ///
+    /// - Parameter key: The key identifying the subscriber to be removed.
     func removeSubscriber(forKey key: String) {
         subscribers.removeValue(forKey: key)
     }
-
+    
+    /// Retrieves all the subscribers currently stored in the coordinator.
+    ///
+    /// - Returns: A collection of all subscribers.
     func allSubscibers() -> Dictionary<String, T>.Values {
         subscribers.values
     }
