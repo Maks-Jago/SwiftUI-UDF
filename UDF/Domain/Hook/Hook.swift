22dc8ca69aaf1411048aed7f885ed98a5c96f7cb
Oksana Fedorchuk <oksanafed.dev@gmail.com>
2024-10-03 15:42:38 +0300

    Documentation for hooks

--- UDF/Domain/Hook/Hook.swift
+++ UDF/Domain/Hook/Hook.swift
@@ -1,12 +1,33 @@
+//===--- Hook.swift ----------------------------------------------===//
+//
+// This source file is part of the UDF open source project
+//
+// Copyright (c) 2024 You Are Launched
+// Licensed under Apache License v2.0
+//
+// See https://opensource.org/licenses/Apache-2.0 for license information
+//
+//===----------------------------------------------------------------------===//
 
 import Foundation
 
+/// A `Hook` is a structure used to define conditional actions (blocks) to be executed within the store, based on changes to the state.
 public struct Hook<State: AppReducer> {
+    /// The unique identifier for the hook.
     let id: AnyHashable
+    /// The type of the hook, defining its behavior (e.g., `default` or `oneTime`).
     let type: HookType
+    /// The condition to be met in the state for the hook's block to execute.
     let condition: (_ state: State) -> Bool
+    /// The block to be executed when the condition is met.
     let block: (_ store: EnvironmentStore<State>) -> Void
     
+    /// Initializes a new `Hook`.
+    /// - Parameters:
+    ///   - id: A unique identifier for the hook.
+    ///   - type: The type of the hook. Default is `.default`.
+    ///   - condition: A closure defining the condition to be met in the state.
+    ///   - block: A closure to be executed when the condition is met.
     public init(
         id: AnyHashable,
         type: HookType = .default,
@@ -19,6 +40,13 @@ public struct Hook<State: AppReducer> {
         self.block = block
     }
     
+    /// A static method to create a new `Hook`.
+    /// - Parameters:
+    ///   - id: A unique identifier for the hook.
+    ///   - type: The type of the hook. Default is `.default`.
+    ///   - condition: A closure defining the condition to be met in the state.
+    ///   - block: A closure to be executed when the condition is met.
+    /// - Returns: A `Hook` instance.
     public static func hook(
         id: AnyHashable,
         type: HookType = .default,
@@ -33,6 +61,12 @@ public struct Hook<State: AppReducer> {
         )
     }
     
+    /// Creates a one-time `Hook` that executes its block only once when the condition is met.
+    /// - Parameters:
+    ///   - id: A unique identifier for the hook.
+    ///   - condition: A closure defining the condition to be met in the state.
+    ///   - block: A closure to be executed when the condition is met.
+    /// - Returns: A `Hook` instance with type `.oneTime`.
     public static func oneTimeHook(
         id: AnyHashable,
         condition: @escaping (_ state: State) -> Bool,
--- UDF/Domain/Hook/HookBuilder.swift
+++ UDF/Domain/Hook/HookBuilder.swift
@@ -1,39 +1,92 @@
+//===--- HookBuilder.swift -----------------------------------------===//
+//
+// This source file is part of the UDF open source project
+//
+// Copyright (c) 2024 You Are Launched
+// Licensed under Apache License v2.0
+//
+// See https://opensource.org/licenses/Apache-2.0 for license information
+//
+//===----------------------------------------------------------------------===//
+
 import Foundation
 
+/// A result builder that facilitates the construction of a collection of `Hook` instances.
+///
+/// `HookBuilder` provides a syntax for aggregating hooks using a Swift result builder, allowing for more readable and flexible hook creation.
 @resultBuilder
 public struct HookBuilder<State: AppReducer> {
+    
+    /// Combines multiple `Hook` components into an array of hooks.
+    ///
+    /// - Parameter components: A variadic list of `Hook` instances.
+    /// - Returns: An array containing the provided `Hook` instances.
     public static func buildBlock(_ components: Hook<State>...) -> [Hook<State>] {
         components
     }
     
+    /// Converts a single `Hook` expression into a `Hook`.
+    ///
+    /// - Parameter expression: A single `Hook` expression.
+    /// - Returns: The same `Hook` instance.
     public static func buildExpression(_ expression: Hook<State>) -> Hook<State> {
         expression
     }
     
+    /// Optionally includes a `Hook` in the resulting collection if it exists.
+    ///
+    /// - Parameter component: An optional `Hook` instance.
+    /// - Returns: An array containing the `Hook` if it exists, otherwise an empty array.
     public static func buildOptional(_ component: Hook<State>?) -> [Hook<State>] {
         component.map { [$0] } ?? []
     }
     
+    /// Chooses between two collections of hooks.
+    ///
+    /// - Parameter component: The first collection of `Hook` instances.
+    /// - Returns: The provided collection of `Hook`.
     public static func buildEither(first component: [Hook<State>]) -> [Hook<State>] {
         component
     }
     
+    /// Chooses between two collections of hooks.
+    ///
+    /// - Parameter component: The second collection of `Hook` instances.
+    /// - Returns: The provided collection of `Hook`.
     public static func buildEither(second component: [Hook<State>]) -> [Hook<State>] {
         component
     }
     
+    /// Flattens an array of arrays of `Hook` instances into a single array.
+    ///
+    /// - Parameter components: An array of arrays containing `Hook` instances.
+    /// - Returns: A flattened array of `Hook` instances.
     public static func buildArray(_ components: [[Hook<State>]]) -> [Hook<State>] {
         components.flatMap { $0 }
     }
     
+    /// Constructs an array containing the first partial block of the `Hook`.
+    ///
+    /// - Parameter first: The first `Hook` instance.
+    /// - Returns: An array containing the first `Hook`.
     public static func buildPartialBlock(first: Hook<State>) -> [Hook<State>] {
         [first]
     }
     
+    /// Combines an accumulated collection of hooks with the next hook.
+    ///
+    /// - Parameters:
+    ///   - accumulated: The accumulated collection of `Hook` instances.
+    ///   - next: The next `Hook` to be added.
+    /// - Returns: An updated array containing all hooks.
     public static func buildPartialBlock(accumulated: [Hook<State>], next: Hook<State>) -> [Hook<State>] {
         accumulated + [next]
     }
     
+    /// Returns the final result of building a collection of `Hook` instances.
+    ///
+    /// - Parameter component: The final array of `Hook` instances.
+    /// - Returns: The final array of `Hook` instances.
     public static func buildFinalResult(_ component: [Hook<State>]) -> [Hook<State>] {
         component
     }
