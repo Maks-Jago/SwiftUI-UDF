f779ab19953fb966561dd29db117bbfa64dc9075
Oksana Fedorchuk <oksanafed.dev@gmail.com>
2024-10-03 15:51:57 +0300

    Protocols

--- UDF/Common/Protocols/Initable.swift
+++ UDF/Common/Protocols/Initable.swift
@@ -1,18 +1,23 @@
+//===--- Initable.swift ----------------------------------------------===//
 //
-//  Initable.swift
-//  
+// This source file is part of the UDF open source project
 //
-//  Created by Max Kuznetsov on 16.02.2021.
+// Copyright (c) 2024 You Are Launched
+// Licensed under Apache License v2.0
 //
+// See https://opensource.org/licenses/Apache-2.0 for license information
+//
+//===----------------------------------------------------------------------===//
 
 import Foundation
 
+/// A protocol representing types that can be initialized with a default initializer.
+/// Conforming types must implement an `init()` method.
 public protocol Initable {
     init()
 }
 
+// Conformances to the `Initable` protocol for standard collection types.
 extension Dictionary: Initable {}
-
 extension Set: Initable {}
-
 extension Array: Initable {}
--- UDF/Common/Protocols/WrappedReducer.swift
+++ UDF/Common/Protocols/WrappedReducer.swift
@@ -1,10 +1,30 @@
+//===--- WrappedReducer.swift ---------------------------------------===//
+//
+// This source file is part of the UDF open source project
+//
+// Copyright (c) 2024 You Are Launched
+// Licensed under Apache License v2.0
+//
+// See https://opensource.org/licenses/Apache-2.0 for license information
+//
+//===----------------------------------------------------------------------===//
+
 import Foundation
 
+/// A protocol representing a reducer that wraps another `Reducing` instance.
+/// It provides a way to contain and manage the state of a wrapped reducer,
+/// allowing for the reduction of actions on the wrapped reducer.
 public protocol WrappedReducer: Reducing {
+    
+    /// The reducer instance that this wrapper manages.
     var reducer: Reducing { get set }
 }
 
 extension WrappedReducer {
+    
+    /// Reduces an action by invoking the `RuntimeReducing` mechanism on the wrapped reducer.
+    /// This method attempts to mutate the state of the wrapped reducer based on the given action.
+    /// - Parameter action: The action to be reduced.
     mutating public func reduce(_ action: some Action) {
         _ = RuntimeReducing.reduce(action, reducer: &reducer)
     }
