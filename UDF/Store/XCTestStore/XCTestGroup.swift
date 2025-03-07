a7c45012f221d71989e0fa3da2335faa1c02c79e
Max Kuznetsov <mx.kuznetsov.dev@gmail.com>
2025-03-07 12:38:36 +0200

    add macos 12 support

--- UDF/Store/XCTestStore/XCTestGroup.swift
+++ UDF/Store/XCTestStore/XCTestGroup.swift
@@ -4,7 +4,9 @@ import os
 
 public final class XCTestGroup {
     static var shared = XCTestGroup()
+#if os(iOS)
     private var group: OSAllocatedUnfairLock<DispatchGroup> = .init(initialState: DispatchGroup())
+#endif
 
     public func enter(
         fileName: String = #file,
@@ -12,9 +14,11 @@ public final class XCTestGroup {
         lineNumber: Int = #line
     ) {
         if ProcessInfo.processInfo.xcTest {
+#if os(iOS)
             group.withLock { group in
                 group.enter()
             }
+#endif
         }
     }
 
@@ -24,9 +28,11 @@ public final class XCTestGroup {
         lineNumber: Int = #line
     ) {
         if ProcessInfo.processInfo.xcTest {
+#if os(iOS)
             group.withLock { group in
                 group.leave()
             }
+#endif
         }
     }
 
@@ -36,7 +42,9 @@ public final class XCTestGroup {
         lineNumber: Int = #line
     ) {
         if ProcessInfo.processInfo.xcTest {
+#if os(iOS)
             _ = group.withLock { $0 }.wait(timeout: .now() + 4)
+#endif
         }
     }
 }
--- UDF/Store/XCTestStore/XCTestStore.swift
+++ UDF/Store/XCTestStore/XCTestStore.swift
@@ -52,6 +52,7 @@ public final class XCTestStore<State: AppReducer> {
         await store.subscribe(build(store))
     }
 
+    @available(macOS 13, *)
     public func subscribe(buildMiddlewares: (_ store: any Store<State>) -> [any Middleware<State>]) async {
         await store.subscribe(buildMiddlewares(store))
     }
@@ -82,6 +83,7 @@ public extension XCTestStore {
 }
 
 public extension XCTestStore {
+    @available(macOS 13, *)
     func subscribe(@MiddlewareBuilder<State> build: (_ store: any Store<State>) -> [MiddlewareWrapper<State>]) async {
         await self.subscribe(buildMiddlewares: { store in
             build(store).map { wrapper in
