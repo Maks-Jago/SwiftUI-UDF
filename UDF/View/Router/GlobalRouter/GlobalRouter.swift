b29619c76c1e612e3325fd3eceac8060aee609d2
kiddden <kazzer@knu.ua>
2025-05-27 16:11:50 +0300

    Revert "test fix"
    This reverts commit 5e677c0264ea9a5a76afd055b78efd133a70754d.

--- UDF/View/Router/GlobalRouter/GlobalRouter.swift
+++ UDF/View/Router/GlobalRouter/GlobalRouter.swift
@@ -133,8 +133,9 @@ public final class GlobalRouter {
     ///   - route: The route to navigate to.
     ///   - router: The router to use for navigation.
     public func resetStack<R: Routing>(to route: R.Route, with router: Router<R>) where R.Route: Hashable {
-        routingPath.wrappedValue = NavigationPath()
-        navigate(to: route, with: router)
+        var newPath = NavigationPath()
+        newPath.append(route)
+        routingPath.wrappedValue = newPath
     }
 }
 
