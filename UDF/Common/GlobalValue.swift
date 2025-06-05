117e444c1ee11b09d30a853ec863cd8b672dce4d
Arthur Zavolovych <boredarthur@gmail.com>
2025-06-05 13:51:44 +0300

    globalvalue as mainactor

--- UDF/Common/GlobalValue.swift
+++ UDF/Common/GlobalValue.swift
@@ -12,16 +12,16 @@
 import Foundation
 
 /// A utility for storing and accessing global singleton values within the application.
-enum GlobalValue {
+@MainActor enum GlobalValue {
     /// A dictionary to store singletons using their type's name as the key.
-    @MainActor private static var values = [String: AnyObject]()
+    private static var values = [String: AnyObject]()
 
     /// Retrieves a stored singleton for the specified type.
     ///
     /// - Parameter vType: The type of the singleton to retrieve.
     /// - Returns: The singleton instance of the specified type.
     /// - Note: This method will crash if the requested singleton is not set prior to this call.
-    @MainActor static func value<T: AnyObject>(for vType: T.Type) -> T {
+    static func value<T: AnyObject>(for vType: T.Type) -> T {
         let key = String(describing: T.self)
         if let singleton = values[key] {
             return singleton as! T
@@ -34,7 +34,7 @@ enum GlobalValue {
     ///
     /// - Parameter value: The singleton instance to store.
     /// - Note: The value is stored using its type's name as the key.
-    @MainActor static func set<T: AnyObject>(_ value: T) {
+    static func set<T: AnyObject>(_ value: T) {
         let key = String(describing: T.self)
         values[key] = value
     }
