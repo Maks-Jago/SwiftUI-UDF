23be1b637fc18c4caf41dcd4ec4f23d03ee88e69
Arthur Zavolovych <boredarthur@gmail.com>
2025-07-16 14:36:20 +0300

    render button using body

--- UDF/View/Dialog/Modifiers/AlertDialogModifier.swift
+++ UDF/View/Dialog/Modifiers/AlertDialogModifier.swift
@@ -40,11 +40,7 @@ struct AlertDialogModifier: ViewModifier {
                     if let alertState {
                         ForEach(Array(alertState.actions.enumerated()), id: \.offset) { _, action in
                             if let button = action as? DialogButton {
-                                Button(button.title, role: button.role) {
-                                    button.action()
-                                    handleAlertDismissal()
-                                }
-                                .disabled(button.disabled)
+                                button.body
                             } else if let textField = action as? DialogTextField {
                                 textField
                             }
