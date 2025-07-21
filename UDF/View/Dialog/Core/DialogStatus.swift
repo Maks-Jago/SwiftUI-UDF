de9272d28401320e1bcbcf1d74a8557c05570a61
Arthur Zavolovych <45713024+boredarthur@users.noreply.github.com>
2025-07-21 19:13:21 +0300

    Fix: Sendable actions (#75)
    * fixed initializer and sendable closures
    
    * fixed formatting

--- UDF/View/Dialog/Core/DialogStatus.swift
+++ UDF/View/Dialog/Core/DialogStatus.swift
@@ -198,7 +198,7 @@ public struct DialogStatus: Equatable, Identifiable, Sendable {
         title: String,
         message: String,
         style: DialogStyle = .alert,
-        @DialogContentBuilder _ actions: @Sendable () -> [any DialogAction]
+        @DialogActionsBuilder _ actions: @Sendable () -> [any DialogAction]
     ) {
         let content = DialogContent(
             title: title,
