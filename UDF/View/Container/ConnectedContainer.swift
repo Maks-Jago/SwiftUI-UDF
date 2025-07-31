242d5c28220be05105a773bde02ec9a04333d956
Arthur Zavolovych <boredarthur@gmail.com>
2025-07-31 18:24:55 +0300

    task

--- UDF/View/Container/ConnectedContainer.swift
+++ UDF/View/Container/ConnectedContainer.swift
@@ -151,10 +151,9 @@ struct ConnectedContainer<C: Component, State: AppReducer>: View {
 
     /// The main view body that renders the component and attaches lifecycle events.
     var body: some View {
-        containerLifecycle.set(didLoad: true, store: store)
-        
         return C(props: map(store))
             .onAppear { onContainerAppear(store) }
             .onDisappear { onContainerDisappear(store) }
+            .task { containerLifecycle.set(didLoad: true, store: store) }
     }
 }
