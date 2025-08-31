47987eb7920c72a9cb4426e441b78366e283f86b
Vlad Andrieiev <fenimors1999@gmail.com>
2025-08-31 18:29:07 +0300

    Fix SwiftTesting detection in TestStore

--- UDF/Common/Extensions/ProcessInfo.swift
+++ UDF/Common/Extensions/ProcessInfo.swift
@@ -10,6 +10,7 @@
 //===----------------------------------------------------------------------===//
 
 import Foundation
+import Testing
 
 public extension ProcessInfo {
     /// A computed property that checks if the current process is running within a test environment.
@@ -24,11 +25,10 @@ public extension ProcessInfo {
     /// }
     /// ```
     var isRunningTests: Bool {
+        Test.current != nil ||
         NSClassFromString("Testing.Test") != nil ||
         environment["XCTestConfigurationFilePath"] != nil ||
         NSClassFromString("XCTestCase") != nil ||
-        environment["SWIFT_TESTING"] != nil ||
-        arguments.contains("test") ||
         Bundle.allBundles.contains { $0.bundlePath.hasSuffix(".xctest") }
     }
 }
