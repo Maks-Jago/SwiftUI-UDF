df94d5392c4962905c4d1d12c944c0029c62439c
Vlad Andrieiev <fenimors1999@gmail.com>
2025-09-04 09:26:02 +0300

    return deleted TestStoreActor

--- UDF/Store/TestStore/TestStore.swift
+++ UDF/Store/TestStore/TestStore.swift
@@ -7,7 +7,13 @@
 
 import Combine
 import SwiftUI
+import Testing
 
+@globalActor public actor TestStoreActor {
+    public private(set) static var shared = TestStoreActor()
+}
+
+@TestStoreActor
 public final class TestStore<State: AppReducer> {
     private struct TestStoreLogger: ActionLogger {
         var actionFilters: [ActionFilter] = [VerboseActionFilter()]
