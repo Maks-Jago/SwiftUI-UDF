12901ffe0a8d60615e8433c0d01d4487833cbc4b
Max Kuznetsov <mx.kuznetsov.dev@gmail.com>
2025-10-06 13:04:01 +0300

    remove serialized from Dialog tests

--- Tests/SwiftUI-UDF-Tests/Dialog/DialogQueueTests.swift
+++ Tests/SwiftUI-UDF-Tests/Dialog/DialogQueueTests.swift
@@ -3,7 +3,7 @@ import SwiftUI
 import Testing
 import UDFSwiftTesting
 
-@Suite(.serialized) struct DialogQueueTests {
+@Suite struct DialogQueueTests {
     // MARK: - Test Helpers
     private func createToastDialog(_ message: String, duration: TimeInterval = 2.0) -> DialogType {
         let config = ToastConfiguration(defaultDuration: duration)
