25e2a87f3568074ce7bb51ed4bd36d6d7a78f76d
Max Kuznetsov <mx.kuznetsov.dev@gmail.com>
2025-10-21 12:11:07 +0300

    Update DialogContent.swift

--- UDF/View/Dialog/Content/DialogContent.swift
+++ UDF/View/Dialog/Content/DialogContent.swift
@@ -290,9 +290,8 @@ public struct DialogContent<Icon: View, CustomContent: View>: Equatable, Sendabl
         lhs.message == rhs.message &&
         lhs.actions.count == rhs.actions.count &&
         zip(lhs.actions, rhs.actions).allSatisfy { lhsAction, rhsAction in
-            // Compare actions by their hash values and types
-            lhsAction.hashValue == rhsAction.hashValue &&
-            type(of: lhsAction) == type(of: rhsAction)
+            // Compare actions by their hash values
+            lhsAction.hashValue == rhsAction.hashValue
         }
     }
 }
