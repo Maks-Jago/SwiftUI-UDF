64f1c8cd25a6cd8fe61fdc0d2a160cf086254b75
Max Kuznetsov <mx.kuznetsov.dev@gmail.com>
2025-12-23 17:05:29 +0200

    add DispatchSpecificKey to BaseMiddleware

--- UDF/_Middleware/_BaseMiddleware.swift
+++ UDF/_Middleware/_BaseMiddleware.swift
@@ -23,6 +23,8 @@ open class _BaseMiddleware<State: AppReducer>: _Middleware, @unchecked Sendable 
 
     /// The dispatch queue used to execute asynchronous operations.
     public var queue: DispatchQueue
+    private let queueKey = DispatchSpecificKey<UUID>()
+    private let queueToken = UUID()
 
     // MARK: - Initialization
 
@@ -34,6 +36,7 @@ open class _BaseMiddleware<State: AppReducer>: _Middleware, @unchecked Sendable 
     public required init(store: some Store<State>, queue: DispatchQueue) {
         self.store = store
         self.queue = queue
+        self.queue.setSpecific(key: queueKey, value: queueToken)
     }
 
     // MARK: - Middleware Status
@@ -385,14 +388,23 @@ open class _BaseMiddleware<State: AppReducer>: _Middleware, @unchecked Sendable 
     }
 
     private func dispatch(action: any Action, filePosition: FileFunctionLineDescription) {
-        queue.async { [weak self] in
-            self?.store.dispatch(
+        if DispatchQueue.getSpecific(key: queueKey) == queueToken {
+            self.store.dispatch(
                 action,
                 fileName: filePosition.fileName,
                 functionName: filePosition.functionName,
                 lineNumber: filePosition.lineNumber
             )
-            if let self {
+            TestGroup.instance(for: self.store).leave()
+        } else {
+            queue.sync { [weak self] in
+                guard let self else { return }
+                self.store.dispatch(
+                    action,
+                    fileName: filePosition.fileName,
+                    functionName: filePosition.functionName,
+                    lineNumber: filePosition.lineNumber
+                )
                 TestGroup.instance(for: self.store).leave()
             }
         }
