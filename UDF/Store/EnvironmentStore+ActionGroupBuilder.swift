e9b9511a085ee0ab30d4bccd19b4bf651d620999
Sashe0 <sashabodnar11@gmail.com>
2026-05-20 13:44:25 +0300

    Add @ActionGroupBuilder overload to dispatch method

---
+++ UDF/Store/EnvironmentStore+ActionGroupBuilder.swift
@@ -0,0 +1,26 @@
+//
+//  EnvironmentStore+ActionGroupBuilder.swift
+//  SwiftUI-UDF
+//
+//  Created by Oleksandr Bodnar on 20.05.2026.
+//
+
+import Foundation
+
+public extension EnvironmentStore {
+    func dispatch(
+        priority: ActionPriority = .default,
+        fileName: String = #file,
+        functionName: String = #function,
+        lineNumber: Int = #line,
+        @ActionGroupBuilder _ builder: () -> ActionGroup
+    ) {
+        dispatch(
+            builder(),
+            priority: priority,
+            fileName: fileName,
+            functionName: functionName,
+            lineNumber: lineNumber
+        )
+    }
+}
