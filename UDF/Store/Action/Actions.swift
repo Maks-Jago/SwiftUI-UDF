9768035faeb9e283ff737f58b141cc142cc09a69
Maksym Horobets <makswinters@outlook.com>
2026-07-08 15:55:11 +0300

    Add parent type in the description of the affected actions

--- UDF/Store/Action/Actions.swift
+++ UDF/Store/Action/Actions.swift
@@ -687,10 +687,10 @@ public extension Actions {
         /// A textual representation of the nested items.
         public var description: String {
             guard shortDescription else {
-                return "DidLoadNestedItems<\(Nested.self)> Parent: \(parentId) (itemsCount: \(items.count), items:\n\t\t\t\t\t\(items.map { String(describing: $0) }.joined(separator: "\n\t\t\t\t\t")))\n"
+                return "DidLoadNestedItems<\(Nested.self)> Parent: \(String(reflecting: ParentId.self))(\(parentId)) (itemsCount: \(items.count), items:\n\t\t\t\t\t\(items.map { String(describing: $0) }.joined(separator: "\n\t\t\t\t\t")))\n"
             }
 
-            return "DidLoadNestedItems<\(Nested.self)> Parent: \(parentId) (count: \(items.count), prefix(1): \(String(describing: items.prefix(1)))"
+            return "DidLoadNestedItems<\(Nested.self)> Parent: \(String(reflecting: ParentId.self))(\(parentId)) (count: \(items.count), prefix(1): \(String(describing: items.prefix(1))))"
         }
     }
 
@@ -731,10 +731,10 @@ public extension Actions {
         public var description: String {
             let parentKeys = dictionary.keys.map { String(describing: $0) }.joined(separator: ", ")
             guard shortDescription else {
-                return "DidLoadNestedByParents<\(Nested.self)> Parents [\(parentKeys)] (parentCount: \(dictionary.keys.count), items:\n\t\t\t\t\t\(dictionary.map { String(describing: $0) }.joined(separator: "\n\t\t\t\t\t")))\n"
+                return "DidLoadNestedByParents<\(Nested.self)> Parents: \(String(reflecting: ParentId.self)) [\(parentKeys)] (parentCount: \(dictionary.keys.count), items:\n\t\t\t\t\t\(dictionary.map { String(describing: $0) }.joined(separator: "\n\t\t\t\t\t")))\n"
             }
 
-            return "DidLoadNestedByParents<\(Nested.self)> Parents [\(parentKeys)] (parentCount: \(dictionary.keys.count), prefix(1): \(String(describing: dictionary.prefix(1)))"
+            return "DidLoadNestedByParents<\(Nested.self)> Parents: \(String(reflecting: ParentId.self)) [\(parentKeys)] (parentCount: \(dictionary.keys.count), prefix(1): \(String(describing: dictionary.prefix(1))))"
         }
     }
 
